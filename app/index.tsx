d1c2bbf7db4a34aad3cbb3bdd52882aefcb2d10b
Shakhawat Salam <shakhawatsalam726@gmail.com>
2026-08-19 18:12:30 +0600

    [feat: nativwind setup is done]

--- app/index.tsx
+++ app/index.tsx
@@ -7,9 +7,10 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      }}>
+      <Text className='text-green-700'>
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
