706a42beb09d1b27948879bf65985fc385a4972d
Shakhawat Salam <shakhawatsalam726@gmail.com>
2026-09-12 13:17:02 +0600

    [feat: onbording screen is added]

--- app/(root)/onboarding.tsx
+++ app/(root)/onboarding.tsx
@@ -1,10 +1,212 @@
-import { View, Text } from "react-native";
+import { ALL_CURRENCIES, CurrencyPicker } from "@/components/CurrencyPicker";
+import { useSupabase } from "@/hooks/useSupabase";
+import {
+  OnboardingFormValues,
+  onboardingSchema,
+} from "@/lib/schemas/onboarding";
+import { useUserStore } from "@/store/useStore";
+import { useUser } from "@clerk/expo";
+import { Feather } from "@expo/vector-icons";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function OnboardingScreen() {
+  const { user } = useUser();
+  const setCurrency = useUserStore((s) => s.setCurrency);
+  const setNeedsOnboarding = useUserStore((s) => s.setNeedsOnbording);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors: formErrors },
+  } = useForm<OnboardingFormValues>({
+    resolver: zodResolver(onboardingSchema),
+    mode: "onBlur",
+    defaultValues: { startingBalance: "" },
+  });
+
+  const [selectedCurrency, setSelectedCurrency] = useState(
+    ALL_CURRENCIES.find((c) => c.code === "INR") ?? ALL_CURRENCIES[0],
+  );
+
+  const [pickerOpen, setPickerOpen] = useState(false);
+  const [saving, setSaving] = useState(false);
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const authSupabase = useSupabase();
+
+  const handleSave = async ({ startingBalance }: OnboardingFormValues) => {
+    const parsed = parseFloat(startingBalance.replace(/,/g, ""));
+    setSaving(true);
+    setError("");
+
+    const { error: updateError } = await authSupabase
+      .from("users")
+      .update({
+        currency: selectedCurrency.code,
+      })
+      .eq("clerk_id", user!.id);
+
+    if (updateError) {
+      setSaving(false);
+      setError("Something went wrong. Please try again 1");
+      return;
+    }
+
+    const { data: defaultAccount, error: accountFetchError } =
+      await authSupabase
+        .from("accounts")
+        .select("id, balance")
+        .eq("user_id", user!.id)
+        .eq("is_default", true)
+        .single();
+
+    if (accountFetchError || !defaultAccount) {
+      setSaving(false);
+      setError("Something went wrong. Please try again 2.");
+      return;
+    }
+
+    const { error: txError } = await authSupabase.from("transactions").insert({
+      user_id: user!.id,
+      account_id: defaultAccount.id,
+      type: "INCOME",
+      amount: parsed,
+      category: "other_income",
+      description: "Starting balance",
+      date: new Date().toISOString(),
+      input_method: "MANUAL",
+    });
+
+    if (txError) {
+      setSaving(false);
+      setError("Something went wrong. Please try again. 3");
+      return;
+    }
+
+    const { error: balanceError } = await authSupabase
+      .from("accounts")
+      .update({ balance: defaultAccount.balance + parsed })
+      .eq("id", defaultAccount.id);
+
+    setSaving(false);
+
+    if (balanceError) {
+      console.log(balanceError, "👋👋👋👋👋👋👋👋");
+      setError("Something went wrong. Please try again. 4");
+      return;
+    }
+
+    setCurrency(selectedCurrency.code);
+    setNeedsOnboarding(false);
+    router.replace("/(root)/(tabs)");
+  };
+
   return (
     <SafeAreaView className='flex-1 bg-brand-body' edges={["top"]}>
-      <Text>OnboardingScreen</Text>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}>
+        <View className='flex-1 justify-center px-6 -mt-16'>
+          <Image
+            source={require("../../assets/images/welth.png")}
+            className='w-36 h-14 mb-8'
+            resizeMode='contain'
+          />
+          <Text className='text-[#1A1D26] text-3xl font-bold mb-2'>
+            Let&apos;s get you set up
+          </Text>
+          <Text className='text-brand-text-muted text-sm mb-10'>
+            A couple of quick details to personalize you experience.
+          </Text>
+
+          <Text className='text-brand-bg text-xs font-medium mb-1.5'>
+            Starting balance
+          </Text>
+
+          <View className='flex-row items-center bg-white border border-[#E8E6DF] rounded-xl px-4 mb-1'>
+            <Text className='text-brand-text-secondary text-sm mr-2'>
+              {selectedCurrency.symbol}
+            </Text>
+            <Controller
+              control={control}
+              name='startingBalance'
+              render={({ field: { value, onChange } }) => (
+                <TextInput
+                  className='flex-1 py-3.5 mb-1.5 text-sm red'
+                  placeholder='e.g 50000'
+                  placeholderTextColor='#8A8D96'
+                  keyboardType='numeric'
+                  returnKeyType='done'
+                  value={value}
+                  onChangeText={(v) => {
+                    setError("");
+                    onChange(v);
+                  }}
+                />
+              )}
+            />
+          </View>
+
+          {formErrors.startingBalance && (
+            <Text className='text-brand-coral mb-4 text-sm'>
+              {formErrors.startingBalance.message}
+            </Text>
+          )}
+          <View className='mb-4' />
+
+          {/* currency picker  */}
+          <Text className='text-brand-bg text-xs font-medium mb-1.5'>
+            Currency
+          </Text>
+
+          <TouchableOpacity
+            onPress={() => setPickerOpen(true)}
+            className='flex-row items-center justify-between bg-white border border-[#E8E6DF] rounded-xl px-4 py-3.5 mb-6'>
+            <Text className='text-sm text-brand-bg'>
+              {selectedCurrency.symbol} {selectedCurrency.code} -{" "}
+              {selectedCurrency.name}
+            </Text>
+            <Feather name='chevron-down' size={16} color='#8A8D96' />
+          </TouchableOpacity>
+
+          {error ? (
+            <Text className='text-brand-coral text-xs mb-4'>{error}ss</Text>
+          ) : null}
+
+          <TouchableOpacity
+            onPress={handleSubmit(handleSave)}
+            disabled={saving}
+            className='bg-brand-bg rounded-xl py-4 items-center'
+            activeOpacity={0.85}>
+            <Text className='text-white text-sm font-semibold'>
+              {saving ? "Saving..." : "Get started"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+      <CurrencyPicker
+        visible={pickerOpen}
+        selectedCode={selectedCurrency.code}
+        onSelect={(currency) => {
+          setSelectedCurrency(currency);
+          setPickerOpen(false);
+        }}
+        onClose={() => setPickerOpen(false)}
+      />
     </SafeAreaView>
   );
 }
